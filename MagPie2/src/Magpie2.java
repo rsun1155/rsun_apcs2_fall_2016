7306e3038b256484d2bc4b22c1fd25dfc6e57e12
rsun1155 <18sunr75@millshighsch.org>
2016-10-05 21:52:44 -0700

    MagPie Part 2

--- MagPie2/src/Magpie2.java
+++ MagPie2/src/Magpie2.java
@@ -20,6 +20,22 @@ public class Magpie2 {
 				|| statement.indexOf("sister") >= 0
 				|| statement.indexOf("brother") >= 0) {
 			response = "Tell me more about your family.";
+		} else if(statement.indexOf("Ms. Bolante") >= 0|| statement.indexOf("Profe Bolante") >=0) {
+			response = "Me encanta el idioma de español. Creo que la Profesora Bolante es fantástica";
+		} else if (statement.indexOf("Ms. Dreyer") >= 0
+				|| statement.indexOf("Ms. Liu") >=0
+				|| statement.indexOf("Ms. Ronina") >=0
+				|| statement.indexOf("Ms. Irish") >= 0 
+				|| statement.indexOf("Ms. Alberta") >=0) {
+			response = "She's a great teacher";
+		} else if (statement.indexOf("music") >=0) { 
+			response = "I love all sorts of music"; 
+		} else if (statement.indexOf("name") >=0){
+			response = "Hello, my name is Inigo Montoya. You killed my father; prepare to die.";
+		} else if (statement.indexOf("food")>=0) {
+			response = "Vietnamese Pho is the best!";
+		} else if (statement.length() <= 0) {
+			response = "If you're alive, please say something."; 
 		} else {
 			response = getRandomResponse();
 		}
@@ -31,7 +47,7 @@ public class Magpie2 {
 	 * returns a non-committal string
 	 */
 	private String getRandomResponse() {
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -44,6 +60,10 @@ public class Magpie2 {
 			response = "Do you really think so?";
 		} else if (whichResponse == 3) {
 			response = "You don't say.";
+		} else if (whichResponse == 4) { 
+			response = "No hablo inglés";
+		} else if (whichResponse == 5) { 
+			response = "Sorry, I can't hear you.";
 		}
 
 		return response;
