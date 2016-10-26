594c3dc5aa279bf843d03fc2f161f1418e60d988
rsun1155 <18sunr75@millshighsch.org>
2016-10-25 22:12:04 -0700

    Split Pt. 1

--- StringSplit/src/Split.java
+++ StringSplit/src/Split.java
@@ -14,6 +14,16 @@ public class Split
 		
 		//play around with String.split! what happens if you "I reallyreally like apples".split("really") ?
 		
+		String [] split = "I like apples!".split(" ");
+		System.out.println(split [0]);
+		split = "I really like really red apples".split("really");
+		System.out.println(split[2]);
+		split = "I reallyreally like apples".split("really"); 
+		System.out.println(split[(split.length-1)]);
+		System.out.println(sandwich("applespineapplesbreadlettustomatobacomayohambreadcheese")); 
+		System.out.println(sandwich("breadbayleafstrawberrybreadcheesemayomustardkiwibreadmelonbananacucumberbread"));
+		
+				
 		 
 	}
 		//Your task:
@@ -21,7 +31,10 @@ public class Split
 		 * use String.split to split up the sandwich by the word "bread" and return what's in the middle of the sandwich and ignores what's on the outside
 		 * What if it's a fancy sandwich with multiple pieces of bread?
 		*/
-		
+		public static String sandwich(String ingredients) {
+			String [] ingredientsArray = ingredients.split("bread");
+			return(ingredientsArray[ingredientsArray.length/2]);
+		}
 		
 		//Your task pt 2:
 		/*Write a method that take in a string like "apples pineapples bread lettus tomato bacon mayo ham bread cheese" describing a sandwich
