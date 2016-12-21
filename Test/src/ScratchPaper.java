77bf79badee3edb55ac0890e5fff41f354a1629e
rsun1155 <18sunr75@millshighsch.org>
2016-12-20 19:52:20 -0800

    PreFinals Scratch Paper Work

--- Test/src/ScratchPaper.java
+++ Test/src/ScratchPaper.java
@@ -1,8 +1,10 @@
-
+import java.util.Arrays;
 public class ScratchPaper {
 	public static void main(String [] args) {
-		System.out.println(3%6);
-		System.out.println(5%24);
+		int [] bunchONums = new int [4];
+		System.out.println(Arrays.toString(bunchONums));
+		String [] bunchONulls = new String [3];
+		System.out.println(Arrays.toString(bunchONulls));
 	}
 
 }
