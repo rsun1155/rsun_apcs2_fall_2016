7c3cf5a0effc115ed32474fa0afd059348a649db
rsun1155 <18sunr75@millshighsch.org>
2017-01-11 22:06:09 -0800

    Testing stuff 1/11/17

--- Test/src/ScratchPaper.java
+++ Test/src/ScratchPaper.java
@@ -1,10 +1,8 @@
 import java.util.Arrays;
 public class ScratchPaper {
 	public static void main(String [] args) {
-		int [] bunchONums = new int [4];
-		System.out.println(Arrays.toString(bunchONums));
-		String [] bunchONulls = new String [3];
-		System.out.println(Arrays.toString(bunchONulls));
+		int [] [] paper = new int [4] [];
+		System.out.print(Arrays.toString(paper[0]));
 	}
 
 }
