5fd57afded575a1439da852db192a9a6cc1d63b6
rsun1155 <18sunr75@millshighsch.org>
2017-01-19 00:25:39 -0800

    More Scratch

--- Test/src/ScratchPaper.java
+++ Test/src/ScratchPaper.java
@@ -1,8 +1,12 @@
 import java.util.Arrays;
 public class ScratchPaper {
 	public static void main(String [] args) {
-		int [] [] paper = new int [4] [];
-		System.out.print(Arrays.toString(paper[0]));
+		int [][] data = new int [4][7];
+		for (int i = 0; i <7; i ++)
+		{
+			data [2][i] = i+1;
+		}
+		System.out.println(Arrays.deepToString(data));
 	}
 
 }
